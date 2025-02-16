68228297b90aed7154c8ca01e03ff721f0c2b9ac
Sardar Shoaib <sardarshoaib910@gmail.com>
2025-02-16 11:09:28 +0500

    dialog boxeshgh

--- JAVASCRIPT/index.js
+++ JAVASCRIPT/index.js
@@ -1,8 +1,18 @@
 //data type====
-console.log(typeof null);
+// console.log(typeof null);
 
 //Dialog boxes====
 // alert("your message");
 // const isConfirm = confirm("hello");
 // const inPut = prompt("Enter name");
 // console.log(inPut);
+
+// String===========>
+let numb = 444;
+let str = `hello from string $${numb} `;
+console.log(str);
+
+let x = 10;
+let y = 21;
+let randomNum = Math.floor(Math.random() * (y - x) + x);
+console.log(randomNum);
