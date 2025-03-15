673c07060a4134ddb9511183f18c10c3d379b31e
Sardar Shoaib <sardarshoaib910@gmail.com>
2025-03-15 12:33:20 +0500

    javascript adv

--- JAVASCRIPT/index.js
+++ JAVASCRIPT/index.js
@@ -8,11 +8,41 @@
 // console.log(inPut);
 
 // String===========>
-let numb = 444;
-let str = `hello from string $${numb} `;
-console.log(str);
+// let numb = 444;
+// let str = `hello from string $${numb} `;
+// // console.log(str);
 
-let x = 10;
-let y = 21;
-let randomNum = Math.floor(Math.random() * (y - x) + x);
-console.log(randomNum);
+// let x = 10;
+// let y = 21;
+// let randomNum = Math.floor(Math.random() * (y - x) + x);
+// console.log(randomNum);
+
+// if Else ===
+// let userName = prompt("please enter your name") || "sardar";
+// console.log(userName);
+// for (var i = 0; i < 3; i++) {
+//   setTimeout(() => console.log(i), 2000);
+// }
+
+//ternory opreator========
+// let data = 5;
+// let message = data === 4 ? "56" : "4";
+// console.log(message);
+
+//memory varibale phase ===>
+
+// let userName = "shoaib";
+// let userAge = 28;
+// let isGraduated = true;
+
+///Object ===
+// let name = "shoaib";
+// let name2 = "shoaib";
+
+// // name===name2  true
+// console.log(name === name2);
+
+// let obj = {};
+// let obj2 = {};
+// //onj ===obj2   false
+// console.log(obj == obj2);
